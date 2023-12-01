882ef0e409266a765011ce265cb9b183fd44c5c0
tomo <tom_friendship@me.com>
2023-12-01 15:12:44 +0900

    fix function

--- tomoEng11Kadai3/ViewController.swift
+++ tomoEng11Kadai3/ViewController.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet private weak var textField1: UITextField!
     @IBOutlet private weak var textField2: UITextField!
     @IBOutlet private weak var label1: UILabel!
     @IBOutlet private weak var label2: UILabel!
     @IBOutlet private weak var resultLabel: UILabel!
+
+    private var n1 = 0
+    private var countN1 = 0
+    private var n2 = 0
+    private var countN2 = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,25 +27,30 @@ class ViewController: UIViewController {
 
     @IBAction func switch1Changed(_ sender: UISwitch) {
 
-        if let n1 = Int(textField1.text!) {
-            textField1.text = String(n1 * -1)
-        }
+        countN1 += 1
     }
 
     @IBAction func switch2Changed(_ sender: UISwitch) {
 
-        if let n2 = Int(textField2.text!) {
-            textField2.text = String(n2 * -1)
-        }
+        countN2 += 1
     }
+
     @IBAction func buttonPressed(_ sender: Any) {
 
-        label1.text = textField1.text!
-        label2.text = textField2.text!
-
-        if let n1 = Int(textField1.text!), let n2 = Int(textField2.text!) {
-
-            resultLabel.text = String(n1 + n2)
+        if countN1 % 2 == 0 {
+            n1 = Int(textField1.text!) ?? 0
+        } else {
+            n1 = (Int(textField1.text!) ?? 0) * -1
         }
+
+        if countN2 % 2 == 0 {
+            n2 = Int(textField2.text!) ?? 0
+        } else {
+            n2 = (Int(textField2.text!) ?? 0) * -1
+        }
+
+        label1.text = String(n1)
+        label2.text = String(n2)
+        resultLabel.text = String(n1 + n2)
     }
 }
